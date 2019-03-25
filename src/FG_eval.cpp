7644866bd6b952f29982ff84aae48de79b8a8554
Safdar Kureishy <safdar.kureishy@gmail.com>
2019-03-25 02:01:00 -0700

    Miscellaneous

--- src/FG_eval.cpp
+++ src/FG_eval.cpp
@@ -73,10 +73,6 @@ void FG_eval::operator()(ADvector& fg, const ADvector& vars) {
         // Actuations at time t:
         AD<double> a = vars[THROTTLE_CTRL_OFFSET + t - 1];
         AD<double> delta = vars[STEER_CTRL_OFFSET + t - 1];
-//        if (t > 1) {   // use previous actuations (to account for latency)
-//            a = vars[THROTTLE_CTRL_OFFSET + t - 2];
-//            delta = vars[STEER_CTRL_OFFSET + t - 2];
-//        }
 
         AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
         AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));
